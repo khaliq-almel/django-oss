378601ae3fc82130a4372aa16bf7eec9550f4c54
khaliq-almel <abdkhaliqalmel@gmail.com>
2019-11-14 21:13:03 +0530

    First commit

---
+++ core/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path
+from .views import (
+    ItemDetailView,
+    CheckoutView,
+    Home,
+    OrderSummaryView,
+    add_to_cart,
+    remove_from_cart,
+    remove_single_item_from_cart,
+    PaymentView,
+    AddCouponView,
+    RequestRefundView,
+    SearchResults,
+    Link
+)
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', Home, name='home'),
+    path('search/', SearchResults, name='search'),
+    path('link/<slug>/',Link,name='link'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('add-coupon/', AddCouponView.as_view(), name='add-coupon'),
+    path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart,
+         name='remove-single-item-from-cart'),
+    path('payment/<payment_option>/', PaymentView.as_view(), name='payment'),
+    path('request-refund/', RequestRefundView.as_view(), name='request-refund')
+]
